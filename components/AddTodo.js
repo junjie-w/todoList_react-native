fdcaf2aee75fe01e80219508cc3b74dd2d70696c
Junjie Wu <junjiewu.brooke@gmail.com>
2020-10-07 18:33:01 +0200

    react native todo list frist part

---
+++ components/AddTodo.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+
+export default function AddTodo({ submitHandler }) {
+  const [text, setText] = useState('')
+  const changeHandler = (val) => {
+    setText(val);
+  }
+
+  return (
+    <View>
+      <TextInput
+        value={text}
+        style={styles.input}
+        placeholder='New todo...'
+        onChangeText={changeHandler}
+      />
+      <Button onPress={() => { submitHandler(text); setText('') }} title='add todo' color='#e5df88' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  }
+})
